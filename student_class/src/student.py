f4225a079307f521aadea46290d8e122e3bd28a5
PAUL DIMITRIS KYRIOU <paulkyriou@PAULs-MacBook-Pro.local>
2023-05-22 17:12:29 +0100

    single class lab_second test

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,10 +1,12 @@
-class Students:
+class Student:
     def __init__(self, name, cohort):
         self.name = name
         self.cohort = cohort 
-        self.talk = "I can talk"
+    
+    def talk(self):
+        return "I can talk!"
+    
+    def say_favourite_language(self, language):
+        return "I love " + language
 
-
-        
-
-        
+    
